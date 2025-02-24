738a448693f5ac4d1cd34dd2cffd8af25137fb00
razrou <34248782-razrou@users.noreply.replit.com>
2025-02-24 10:47:32 +0000

    Agent query: Kunt u nu de registratie sectie zien onder het login formulier met de tekst 'Wil je vrijwilliger worden bij MEFEN?' en een registratie knop?
    Remove debug elements from login page
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/a74672c6-b76b-4d3d-af48-fb8e2832d3f6/b5eebcfb-0d31-405a-98e6-beb2e5d63eba.jpg

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -112,11 +112,6 @@ export default function Login() {
               </Button>
             </form>
 
-            {/* DEBUG ELEMENT */}
-            <div style={{ backgroundColor: "red", color: "white", padding: "10px", margin: "10px 0", textAlign: "center" }}>
-              DEBUG: Registration Section Start
-            </div>
-
             {/* Registratie sectie */}
             <div className="mt-8 pt-6 border-t border-gray-200">
               <div className="text-center">
@@ -134,11 +129,6 @@ export default function Login() {
                   Registreer je als vrijwilliger
                 </Button>
               </div>
-            </div>
-
-            {/* DEBUG ELEMENT */}
-            <div style={{ backgroundColor: "red", color: "white", padding: "10px", margin: "10px 0", textAlign: "center" }}>
-              DEBUG: Registration Section End
             </div>
           </CardContent>
         </Card>
