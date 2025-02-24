7ea374e71e55938c2dac03e089ebd61b9800997c
razrou <34248782-razrou@users.noreply.replit.com>
2025-02-24 15:25:40 +0000

    Agent query: Controleer of de kalenderdagen (ma, di, wo, etc.) nu correct zijn uitgelijnd met de juiste datums. Bijvoorbeeld: staat 'ma' nu boven maandag 24 februari?
    Fix: Correct calendar ISOWeek setting
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/a74672c6-b76b-4d3d-af48-fb8e2832d3f6/a3c9824f-2dac-4bd6-a58e-a7ba2c92a32c.jpg

--- client/src/components/ui/calendar.tsx
+++ client/src/components/ui/calendar.tsx
@@ -62,6 +62,7 @@ function Calendar({
         IconLeft: () => <ChevronLeft className="h-4 w-4" />,
         IconRight: () => <ChevronRight className="h-4 w-4" />,
       }}
+      ISOWeek={true}
       {...props}
     />
   )
